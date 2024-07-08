dc6d36494e9639051b5bca8a84df06bf5818c597
Krizia Bianca <krizia.dev@gmail.com>
2024-07-09 00:02:56 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+  "use strict";
+  
+	// SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    }); 
+})(jQuery);
